396372db88f5cde1d31794b864851e81d0da8d1f
Justus Sydänmaa <107138154+Jyddys@users.noreply.github.com>
2022-10-01 13:32:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+var password=document.getElementById("password1");
+var password=document.getElementById("password2");
+var password=document.getElementById("password3");
+var password=document.getElementById("password4");
+
+function genPassword(){
+genPassword1()
+genPassword2()
+genPassword3()
+genPassword4()
+}
+
+ function genPassword1() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+   document.getElementById("password1").value = password;
+
+  } 
+ 
+}
+
+function genPassword2() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+   document.getElementById("password2").value = password;
+
+  } 
+ 
+}
+
+function genPassword3() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+   document.getElementById("password3").value = password;
+
+  } 
+ 
+}
+
+function genPassword4() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+   document.getElementById("password4").value = password;
+
+  } 
+ 
+}
+
